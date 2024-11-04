e3c93094c5576e42d3112a88040b1c150f98ee39
zzinnu <117358809+zzinnu@users.noreply.github.com>
2024-11-04 13:40:49 +0900

    ㅁㄴㅇㄹ
    1. using 정리

--- Assets/+---Code/I +---Scripts/II +---UI/III +---StatusUI/ConfirmButtonUI.cs
+++ Assets/+---Code/I +---Scripts/II +---UI/III +---StatusUI/ConfirmButtonUI.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 public enum StatusType
